bada233f50d115bd71b1c0d26aa2971004f3894f
wuws <it-wuws@derucci.net>
2026-01-27 14:52:57 +0800

    Initial Commit

---
+++ Xmusic/XmusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  XmusicApp.swift
+//  Xmusic
+//
+//  Created by will on 2026/1/27.
+//
+
+import SwiftUI
+
+@main
+struct XmusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
